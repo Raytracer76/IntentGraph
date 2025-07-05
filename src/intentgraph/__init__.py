78fbbee184e5c48aa85a4597e96e94293a6da4ee
Nicolas Ligas <nligas@gmail.com>
2025-07-05 21:18:45 +0200

    Version bumped to 0.2.0

--- src/intentgraph/__init__.py
+++ src/intentgraph/__init__.py
@@ -1,8 +1,8 @@
 """IntentGraph - A best-in-class repository dependency analyzer."""
 
-__version__ = "0.1.0"
-__author__ = "Your Name"
-__email__ = "your.email@example.com"
+__version__ = "0.2.0"
+__author__ = "Nicolas Ligas"
+__email__ = "nligas@gmail.com"
 
 from .domain.models import AnalysisResult, FileInfo, Language, LanguageSummary
 
